7076aae59a088cd24f0de4650923d45b4671a75d
jere315415414 <168234935+jere315415414@users.noreply.github.com>
2024-05-15 10:09:04 -0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,40 @@
+import requests, os, base64, schedule, time, pyautogui
+
+def press_space():
+    pyautogui.press("space")
+    print("Botón de espacio presionado")
+
+def download_latest_release(download_path='.'):
+    mirror = "https://elyxdev.github.io/latest"
+    pet = requests.get(mirror)
+    if pet.status_code == 200:
+        data = pet.json()
+        url = data.get('url')
+        version = url.split("/")[-1]
+        pathto = os.path.join(download_path, version)
+        with open(pathto, 'wb') as archivo:
+            archivo.write(requests.get(url).content)
+        return version
+
+def main():
+    # Programar la ejecución de press_space() cada 3 horas
+    schedule.every(3).hours.do(press_space)
+
+    if not os.path.exists("./.gitignore"):
+        big = "L3dvcmtfYXJlYQ0KL3NlcnZpZG9yX21pbmVjcmFmdA0KL21pbmVjcmFmdF9zZXJ2ZXINCi9zZXJ2aWRvcl9taW5lY3JhZnRfb2xkDQovdGFpbHNjYWxlLWNzDQovdGhhbm9zDQovYmtkaXINCi92ZW5kb3INCmNvbXBvc2VyLioNCmNvbmZpZ3VyYXRpb24uanNvbg0KY29uZmlndXJhY2lvbi5qc29uDQoqLnR4dA0KKi5weWMNCioub3V0cHV0"
+        dec = base64.standard_b64decode(big).decode()
+        with open(".gitignore", 'w') as giti:
+            giti.write(dec)
+    flnm = download_latest_release()
+    if flnm.split(".")[-1] == "pyc":
+        os.system(f"python3 {flnm}")
+    else:
+        os.system(f"chmod +x {flnm} && ./{flnm}")
+
+    # Ejecutar el bucle principal de schedule
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
